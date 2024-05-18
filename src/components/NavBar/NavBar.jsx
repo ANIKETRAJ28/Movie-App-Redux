8200db04c03e3e847701e971713deebfba13db0d
Aniket <aniketraj28042003@gmail.com>
2024-05-18 11:09:47 +0530

    added autocomplete search bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,11 @@
+import { /*useRef*/ useState } from 'react';
 import './NavBar.css';
 
 function NavBar() {
+
+    // const resultListRef = useRef(null);
+    const [searchResult, setSearchResult] = useState(false);
+
     return (
         <div className="nav-bar-wrapper">
             <div>Movie Base</div>
@@ -8,7 +13,20 @@ function NavBar() {
                 <input
                     type="text"
                     placeholder='Search movie...'
+                    onFocus={() => {
+                        // resultListRef.current.style.display = "block";
+                        setSearchResult(true);
+                    }}
+                    onBlurCapture={() => {
+                        // resultListRef.current.style.display = "none";
+                        setSearchResult(false);
+                    }}
                 />
+                <div id='result-list' /*ref={resultListRef}*/ style={{display: `${searchResult ? 'block' : 'none'}`}}>
+                    <div>result 1</div>
+                    <div>result 2</div>
+                    <div>result 3</div>
+                </div>
             </div>
             <div>
                 Theme
